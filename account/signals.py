70a63f5c11aea5b0130e64e14931ae18e989914d
QuantlyFinance <florent@quantly.finance>
2022-10-04 10:12:43 +0200

    Initial commit

--- account/signals.py
+++ account/signals.py
@@ -1,7 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from account.models import Account
-from account.tasks import fetch_account_balance, fetch_positions
 import structlog
 
 log = structlog.get_logger(__name__)
@@ -9,12 +8,4 @@ log = structlog.get_logger(__name__)
 
 @receiver(post_save, sender=Account)
 def account_saved(sender, instance, created, raw, using, **kwargs):
-
-    log.info('Signal received', signal='account saved')
-
-    if created:
-
-        log.info('Fetch account information', account=instance.name)
-
-        fetch_account_balance.delay(instance.id)
-        fetch_positions.delay(instance.id)
+    pass
