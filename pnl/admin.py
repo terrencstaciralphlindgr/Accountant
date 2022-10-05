28542c2dfa1392fbf9eabb66fc44fb06faeaf0dc
QuantlyFinance <florent@quantly.finance>
2022-10-05 17:26:00 +0200

    Initial commit

--- pnl/admin.py
+++ pnl/admin.py
@@ -7,9 +7,19 @@ admin.site.enable_nav_sidebar = False
 
 @admin.register(Inventory)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('trade', 'account', 'exchange', 'currency', 'instrument', 'stock', 'total_cost', 'average_cost',
-                    'realized_pnl', 'unrealized_pnl', 'datetime')
+    list_display = ('trade', 'get_side', 'account', 'exchange', 'currency', 'instrument', 'stock', 'total_cost',
+                    'average_cost', 'realized_pnl', 'unrealized_pnl', 'datetime')
     readonly_fields = ('trade', 'id', 'account', 'exchange', 'currency', 'instrument', 'stock', 'total_cost',
                        'average_cost', 'realized_pnl', 'unrealized_pnl', 'datetime')
     ordering = ('-datetime',)
     actions = ['', ]
+    list_filter = (
+        ('account', admin.RelatedOnlyFieldListFilter),
+        ('exchange', admin.RelatedOnlyFieldListFilter),
+        'instrument',
+    )
+
+    def get_side(self, obj):
+        return obj.trade.side
+
+    get_side.short_description = 'Side'
