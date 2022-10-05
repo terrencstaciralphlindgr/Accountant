f7a0967eb7ca749a616e58af70c3fa95806f2838
QuantlyFinance <florent@quantly.finance>
2022-10-05 13:27:48 +0200

    Initial commit

--- account/models.py
+++ account/models.py
@@ -112,6 +112,8 @@ class Trade(TimestampedModel):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='trade', blank=True, null=True)
     tradeid = models.CharField(max_length=200)
     symbol = models.CharField(max_length=20)
+    side = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
     datetime = models.DateTimeField()
     timestamp = models.BigIntegerField()
     taker_or_maker = models.CharField(max_length=20)
