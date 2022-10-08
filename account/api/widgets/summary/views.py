214d1598ac4d7d368e37084b994e7aa0dd85554a
QuantlyFinance <florent@quantly.finance>
2022-10-08 20:32:14 +0200

    fix

--- account/api/widgets/summary/views.py
+++ account/api/widgets/summary/views.py
@@ -1,15 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import permission_classes
-from rest_framework.permissions import IsAdminUser
+from rest_framework import permissions
 from account.models import Balance
 import structlog
 
 log = structlog.get_logger(__name__)
 
 
-@permission_classes([IsAdminUser])
 class AssetValueViewSet(APIView):
+    """
+    Returns True if an order ID is valid else False.
+    * Only admin users are able to access this view.
+    """
+    permission_classes = [permissions.IsAdminUser]
+
     def get(self, request, account_id):
         asset_value = Balance.objects.filter(account__id=account_id).latest('dt').assets_total_value
         Response(dict(asset_value=asset_value, growth=0))
