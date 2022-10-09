6911b128dc0defa6af72356e1054eb8270e2eae0
QuantlyFinance <florent@quantly.finance>
2022-10-09 20:06:12 +0200

    fix

--- accountant/settings.py
+++ accountant/settings.py
@@ -216,11 +216,12 @@ structlog.configure(
         # structlog.processors.StackInfoRenderer(),
         # structlog.processors.format_exc_info,
         # structlog.processors.UnicodeDecoder(),
-        structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+        structlog.wrap_logger,
+        # structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
     ],
     # context_class=dict,
     # wrapper_class=structlog.stdlib.BoundLogger,
-    # logger_factory=structlog.stdlib.LoggerFactory(),
+    logger_factory=structlog.stdlib.LoggerFactory(),
     # cache_logger_on_first_use=True,
 )
 
