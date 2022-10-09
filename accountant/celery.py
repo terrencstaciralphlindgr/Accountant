837e5b2a8f1077c325fe3b7a1cc280375a28391c
QuantlyFinance <florent@quantly.finance>
2022-10-09 19:37:51 +0200

    fix

--- accountant/celery.py
+++ accountant/celery.py
@@ -107,9 +107,9 @@ def receiver_setup_logging(loglevel, logfile, format, colorize, **kwargs):  # pr
         processors=[
             # structlog.contextvars.merge_contextvars,
             # structlog.stdlib.filter_by_level,
-            structlog.processors.TimeStamper(fmt="iso"),
+            # structlog.processors.TimeStamper(fmt="iso"),
             # structlog.stdlib.add_logger_name,
-            structlog.stdlib.add_log_level,
+            # structlog.stdlib.add_log_level,
             # structlog.stdlib.PositionalArgumentsFormatter(),
             # structlog.processors.StackInfoRenderer(),
             # structlog.processors.format_exc_info,
--- accountant/settings.py
+++ accountant/settings.py
@@ -211,7 +211,7 @@ structlog.configure(
         # structlog.stdlib.filter_by_level,
         structlog.processors.TimeStamper(fmt="iso"),
         # structlog.stdlib.add_logger_name,
-        # structlog.stdlib.add_log_level,
+        structlog.stdlib.add_log_level,
         # structlog.stdlib.PositionalArgumentsFormatter(),
         # structlog.processors.StackInfoRenderer(),
         # structlog.processors.format_exc_info,
