8e2ed761b3e7ca81dc0e36037af5bcbfbbb03cba
QuantlyFinance <florent@quantly.finance>
2022-10-09 19:55:08 +0200

    fix

--- pnl/tasks.py
+++ pnl/tasks.py
@@ -6,7 +6,7 @@ from accountant.methods import datetime_directive_ISO_8601
 from accountant.celery import app
 from pnl.models import Inventory
 
-logger = structlog.get_logger("foo")
+logger = structlog.get_logger("pp")
 logger.try_unbind('task_id', 'parent_task_id', 'request_id', 'user_id', 'ip',)
 
 
