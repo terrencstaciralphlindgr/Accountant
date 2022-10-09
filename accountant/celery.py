c5be73b03d5c3951881fcf8c7c1e48f3eb383447
QuantlyFinance <florent@quantly.finance>
2022-10-09 17:52:41 +0200

    fix

--- accountant/celery.py
+++ accountant/celery.py
@@ -123,7 +123,7 @@ def receiver_setup_logging(loglevel, logfile, format, colorize, **kwargs):  # pr
 
 @receiver(bind_extra_request_metadata)
 def bind_unbind_metadata(request, logger, **kwargs):
-    logger.unbind('task_id')  # , 'ip', 'user_id')  # 'request_id',
+    structlog.contextvars.unbind_contextvars('task_id')  # , 'ip', 'user_id')  # 'request_id',
 
 
 @receiver(signals.modify_context_before_task_publish)
