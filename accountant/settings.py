4171577be598d16bc71c71b3ffe7e53fa2e7509e
QuantlyFinance <florent@quantly.finance>
2022-10-09 16:41:57 +0200

    fix

--- accountant/settings.py
+++ accountant/settings.py
@@ -205,20 +205,15 @@ LOGGING = {
 
 structlog.configure(
     processors=[
-        structlog.stdlib.filter_by_level,
         structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S.%f"),  # (fmt="iso"),
-        structlog.stdlib.add_logger_name,
-        structlog.stdlib.add_log_level,
-        structlog.stdlib.PositionalArgumentsFormatter(),
         structlog.processors.StackInfoRenderer(),
         structlog.processors.format_exc_info,
         structlog.processors.UnicodeDecoder(),
-        structlog.processors.ExceptionPrettyPrinter(),
-        structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+        structlog.processors.ExceptionPrettyPrinter()
     ],
     context_class=structlog.threadlocal.wrap_dict(dict),
-    logger_factory=structlog.stdlib.LoggerFactory(),
-    wrapper_class=structlog.stdlib.BoundLogger,
+    # logger_factory=structlog.LoggerFactory(),
+    wrapper_class=structlog.BoundLogger,
     cache_logger_on_first_use=True,
 
     # processors=[
