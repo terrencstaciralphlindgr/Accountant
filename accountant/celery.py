2cafdc8c053a8067750292d2779ef1e5bfd519e6
QuantlyFinance <florent@quantly.finance>
2022-10-09 14:13:10 +0200

    fix

--- accountant/celery.py
+++ accountant/celery.py
@@ -114,24 +114,24 @@ def receiver_setup_logging(loglevel, logfile, format, colorize, **kwargs):
     #     }
     # )
 
-    structlog.configure(
-        processors=[
-            structlog.stdlib.filter_by_level,
-            structlog.processors.TimeStamper(fmt="iso"),  # key=
-            structlog.stdlib.add_logger_name,
-            structlog.stdlib.add_log_level,
-            structlog.stdlib.PositionalArgumentsFormatter(),
-            structlog.processors.StackInfoRenderer(),
-            structlog.processors.format_exc_info,
-            structlog.processors.UnicodeDecoder(),
-            structlog.processors.ExceptionPrettyPrinter(),
-            structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
-        ],
-        context_class=structlog.threadlocal.wrap_dict(dict),
-        logger_factory=structlog.stdlib.LoggerFactory(),
-        wrapper_class=structlog.stdlib.BoundLogger,
-        cache_logger_on_first_use=True,
-    )
+    # structlog.configure(
+    #     processors=[
+    #         structlog.stdlib.filter_by_level,
+    #         structlog.processors.TimeStamper(fmt="iso"),  # key=
+    #         structlog.stdlib.add_logger_name,
+    #         structlog.stdlib.add_log_level,
+    #         structlog.stdlib.PositionalArgumentsFormatter(),
+    #         structlog.processors.StackInfoRenderer(),
+    #         structlog.processors.format_exc_info,
+    #         structlog.processors.UnicodeDecoder(),
+    #         structlog.processors.ExceptionPrettyPrinter(),
+    #         structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+    #     ],
+    #     context_class=structlog.threadlocal.wrap_dict(dict),
+    #     logger_factory=structlog.stdlib.LoggerFactory(),
+    #     wrapper_class=structlog.stdlib.BoundLogger,
+    #     cache_logger_on_first_use=True,
+    # )
 
 
 # @receiver(bind_extra_request_metadata)
