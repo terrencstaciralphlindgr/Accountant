b98841d5e5b6fe19d3824dbe4851241df47d8f49
QuantlyFinance <florent@quantly.finance>
2022-10-10 10:28:31 +0200

    fix

--- account/api/widgets/summary/views.py
+++ account/api/widgets/summary/views.py
@@ -15,14 +15,10 @@ class AssetValueViewSet(APIView):
     def get(self, request, account_id):
 
         period = request.GET.get('period')
-        if period:
 
-            log.info('Return asset value', period=period)
+        log.info('Return asset value', period=period)
 
-            asset_value = Balance.objects.filter(account__id=account_id).latest('dt').assets_total_value
-            growth = Account.objects.get(id=account_id).growth(period)
-            return Response(dict(asset_value=asset_value, growth=growth))
+        asset_value = Balance.objects.filter(account__id=account_id).latest('dt').assets_total_value
+        growth = Account.objects.get(id=account_id).growth(period)
+        return Response(dict(asset_value=asset_value, growth=growth))
 
-        else:
-            log.error('Parameter ?str:period is required')
-            raise ValidationError({'Please enter a valid ?str:period parameter.'})
