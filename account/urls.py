e2d5c68728b556a46fdcd63def6e6a637a8f85c0
QuantlyFinance <florent@quantly.finance>
2022-10-11 12:02:03 +0200

    fix

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from account.api.views import AccountViewSet, BalanceViewSet
-from account.api.widgets.summary.views import AssetValueViewSet, AssetGrowthViewSet, ExpositionViewSet
+from account.api.widgets.summary.views import *
 
 
 router = routers.DefaultRouter()
@@ -13,4 +13,5 @@ urlpatterns = [
     path('account/<int:account_id>/summary/asset_value/', AssetValueViewSet.as_view(), name='asset_value'),
     path('account/<int:account_id>/summary/asset_growth/', AssetGrowthViewSet.as_view(), name='asset_growth'),
     path('account/<int:account_id>/summary/exposition/', ExpositionViewSet.as_view(), name='exposition'),
+    path('account/<int:account_id>/summary/history/', HistoricalValueViewSet.as_view(), name='exposition'),
 ]
