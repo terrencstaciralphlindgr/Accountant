761b71769c4047a61cb46405d20ea46a5d9432cd
QuantlyFinance <florent@quantly.finance>
2022-10-11 16:54:21 +0200

    fix

--- account/tasks.py
+++ account/tasks.py
@@ -19,12 +19,7 @@ import celery
 logger = structlog.wrap_logger(
     celery.utils.log.get_task_logger(__name__),
     processors=[
-        structlog.contextvars.merge_contextvars,
-        structlog.stdlib.filter_by_level,
         structlog.processors.TimeStamper(fmt="iso"),
-        structlog.stdlib.add_logger_name,
-        structlog.stdlib.add_log_level,
-        structlog.stdlib.PositionalArgumentsFormatter(),
         structlog.processors.StackInfoRenderer(),
         structlog.processors.format_exc_info,
         structlog.processors.UnicodeDecoder()
