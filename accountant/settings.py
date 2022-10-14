2b8aee1fe151822d35367e25f4bdf0a35994bdde
QuantlyFinance <florent@quantly.finance>
2022-10-14 17:03:45 +0200

    fix

--- accountant/settings.py
+++ accountant/settings.py
@@ -161,11 +161,11 @@ LOGGING = {
         },
     },
     "loggers": {
-        "": {
-            "handlers": ["console", "flat_line_file", "json_file"],
-            "level": "INFO",
-            'propagate': False
-        },
+        # "": {
+        #     "handlers": ["console", "flat_line_file", "json_file"],
+        #     "level": "INFO",
+        #     'propagate': False
+        # },
         # "django_structlog": {
         #     "handlers": ["console", "flat_line_file", "json_file"],
         #     "level": "INFO",
