fb098170c0413faf02775a4aae1a7c72eb1340a8
QuantlyFinance <florent@quantly.finance>
2022-10-14 17:06:29 +0200

    fix

--- accountant/celery.py
+++ accountant/celery.py
@@ -95,16 +95,16 @@ def receiver_setup_logging(loglevel, logfile, format, colorize, **kwargs):  # pr
                 #     "level": "DEBUG",
                 #     'propagate': False
                 # },
-                # "market": {
-                #     "handlers": ["console", "flat_line_file", "json_file"],
-                #     "level": "INFO",
-                #     'propagate': False
-                # },
-                # "account": {
-                #     "handlers": ["console", "flat_line_file", "json_file"],
-                #     "level": "INFO",
-                #     'propagate': False
-                # },
+                "market": {
+                    "handlers": ["console"],  # , "flat_line_file", "json_file"],
+                    "level": "INFO",
+                    'propagate': False
+                },
+                "account": {
+                    "handlers": ["console"],  # , "flat_line_file", "json_file"],
+                    "level": "INFO",
+                    'propagate': False
+                },
                 # "statistics": {
                 #     "handlers": ["console", "flat_line_file", "json_file"],
                 #     "level": "INFO",
