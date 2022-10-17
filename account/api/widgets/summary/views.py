2ee6ed0697862405b91410ce112c1c5ca4fa7927
QuantlyFinance <florent@quantly.finance>
2022-10-17 11:23:59 +0200

    fix

--- account/api/widgets/summary/views.py
+++ account/api/widgets/summary/views.py
@@ -98,8 +98,7 @@ class RecentTradesViewSet(APIView):
         account = Account.objects.get(id=account_id)
         start_datetime = get_start_datetime(account, period)
 
-        qs = Trade.objects.filter(account=account,
-                                  datetime__gte=start_datetime).annotate(
-            date_only=Cast('datetime', DateTimeField())).defer('info').values().order_by('-datetime')
+        qs = Trade.objects.filter(account=account, datetime__gte=start_datetime).defer('info').annotate(
+            date_only=Cast('datetime', DateTimeField())).values().order_by('-datetime')
 
         return Response(qs)
