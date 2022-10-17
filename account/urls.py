8fddc7391ee1b189d6882eeda8ae4a86a104a03c
QuantlyFinance <florent@quantly.finance>
2022-10-17 12:02:48 +0200

    fix

--- account/urls.py
+++ account/urls.py
@@ -10,10 +10,11 @@ router.register('balance', BalanceViewSet, basename="balances-list")
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('account/<int:account_id>/summary/value/', AssetValueViewSet.as_view()),
-    path('account/<int:account_id>/summary/growth/', AssetGrowthViewSet.as_view()),
-    path('account/<int:account_id>/summary/exposition/', ExpositionViewSet.as_view()),
+    path('account/<int:account_id>/summary/assets/', AssetsViewSet.as_view()),
+    path('account/<int:account_id>/summary/assets_value/', AssetValueViewSet.as_view()),
+    path('account/<int:account_id>/summary/assets_growth/', AssetGrowthViewSet.as_view()),
+    path('account/<int:account_id>/summary/assets_exposition/', ExpositionViewSet.as_view()),
     path('account/<int:account_id>/summary/historical_value/', HistoricalValueViewSet.as_view()),
     path('account/<int:account_id>/summary/historical_weight/', HistoricalWeightsViewSet.as_view()),
-    path('account/<int:account_id>/summary/recent_trades/', RecentTradesViewSet.as_view()),
+    path('account/<int:account_id>/summary/historical_trades/', RecentTradesViewSet.as_view()),
 ]
